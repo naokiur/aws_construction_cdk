f63533f1c53e993d890290b1d9685a818ea08913
naokiur <atomic.transfer.comp@gmail.com>
2020-01-05 19:27:56 +0900

    add pipeline for deploy by Cfn.

--- src/main/java/jp/ne/naokiur/CICDStack.java
+++ src/main/java/jp/ne/naokiur/CICDStack.java
@@ -1,18 +1,18 @@
 package jp.ne.naokiur;
 
 import software.amazon.awscdk.core.*;
+import software.amazon.awscdk.services.codebuild.PipelineProject;
 import software.amazon.awscdk.services.codepipeline.Artifact;
 import software.amazon.awscdk.services.codepipeline.Pipeline;
 import software.amazon.awscdk.services.codepipeline.StageProps;
 import software.amazon.awscdk.services.codepipeline.actions.Action;
+import software.amazon.awscdk.services.codepipeline.actions.CodeBuildAction;
 import software.amazon.awscdk.services.codepipeline.actions.GitHubSourceAction;
 import software.amazon.awscdk.services.codepipeline.actions.S3DeployAction;
-import software.amazon.awscdk.services.codepipeline.actions.S3SourceAction;
 import software.amazon.awscdk.services.s3.Bucket;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class CICDStack extends Stack {
 
@@ -23,8 +23,6 @@ public class CICDStack extends Stack {
     public CICDStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // CodePipeLine作成（S3格納用）
-        // Source
         final Artifact sourceArtifact = Artifact.artifact("Source");
 
 //        final SecretValue githubToken = SecretValue.ssmSecure(
@@ -54,17 +52,38 @@ public class CICDStack extends Stack {
                 .actions(new ArrayList<>(Arrays.asList(github)))
                 .build();
 
+        // build
+        final PipelineProject codeBuildProject = PipelineProject.Builder
+                .create(this, "GithubSourceBuild")
+                .projectName("codeBuildAction")
+                .build();
+
+        final Artifact buildArtifact = Artifact.artifact("Build");
+        final Action codeBuildAction = CodeBuildAction.Builder
+                .create()
+                .actionName("BuildSource")
+                .project(codeBuildProject)
+                .input(sourceArtifact)
+                .outputs(new ArrayList<>(Arrays.asList(buildArtifact)))
+                .build();
+
+        final StageProps build = StageProps.builder()
+                .stageName("BuildSource")
+                .actions(new ArrayList<>(Arrays.asList(codeBuildAction)))
+                .build();
+
         // deploy
         final Bucket deployBucket = Bucket.Builder
                 .create(this, "deployS3")
                 .bucketName("naokiur-deploy-bucket")
+                .versioned(true)
                 .build();
 
         final Action s3 = S3DeployAction.Builder
                 .create()
                 .bucket(deployBucket)
                 .actionName("DeploySourceToS3")
-                .input(sourceArtifact)
+                .input(buildArtifact)
                 .build();
 
         final StageProps deploy = StageProps.builder()
@@ -75,13 +94,7 @@ public class CICDStack extends Stack {
         final Pipeline saveToS3Pipeline = Pipeline.Builder
                 .create(this, "saveSourceToS3")
                 .pipelineName("saveSourceToS3")
-                .stages(new ArrayList<>(Arrays.asList(source, deploy)))
+                .stages(new ArrayList<>(Arrays.asList(source, build, deploy)))
                 .build();
-
-        // Deploy
-
-        // CodePipeline作成（デプロイ用）
-        //  * S3からソース取得
-        //  * EBにデプロイ
     }
 }
